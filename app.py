679b032124a4b875ed15eaa7c5f44ee103d58ff9
a-46 <75574153+a-46@users.noreply.github.com>
2021-03-16 10:52:00 +0800

    buttons added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ Faker.seed(0)
 
 app = Flask(__name__)
 
-
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 
 
 
@@ -104,10 +104,10 @@ def index():
         title = request.form.get('title')
         year = request.form.get('year')
 
-        if not title or not year or len(year) > 4 len(title) > 60:
+        if not title or not year or len(year) > 4 or len(title) > 60:
             flash('Invalid input.')
             return redirect(url_for('index'))
-        movie=Movie(title = title, year year)
+        movie=Movie(title = title, year= year)
         db.session.add(movie)
         db,session.commit()
         flash('item created')
@@ -115,3 +115,33 @@ def index():
 
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
